0ef4b767826b3353af95afca36d8d470adfe2ba4
Athou <jeremiepanzer@gmail.com>
2021-11-13 16:33:50 +0100

    add "quote random" command

--- src/commands/quote.rs
+++ src/commands/quote.rs
@@ -1,8 +1,10 @@
 use crate::commands::SlashCommand;
 use anyhow::anyhow;
 use anyhow::Error;
+use rand::Rng;
 use serenity::async_trait;
 use serenity::builder::CreateApplicationCommand;
+use serenity::model::interactions::application_command::ApplicationCommandInteractionDataOption;
 use serenity::model::interactions::application_command::ApplicationCommandInteractionDataOptionValue;
 use serenity::model::interactions::application_command::ApplicationCommandOptionType;
 use serenity::model::interactions::application_command::ApplicationCommandType;
@@ -30,6 +32,53 @@ impl QuoteCommand {
             .await?;
         Ok(quote.map(|q| q.quote))
     }
+
+    // TODO remove duplicate count method
+    async fn count(&self) -> Result<i64, Error> {
+        let count: i64 = sqlx::query("SELECT count(*) from Quote")
+            .fetch_one(&*self.db_pool)
+            .await?
+            .get(0);
+        Ok(count)
+    }
+
+    async fn trigger_get(
+        &self,
+        command: &ApplicationCommandInteractionDataOption,
+    ) -> Result<Option<String>, Error> {
+        let option = command
+            .options
+            .get(0)
+            .ok_or(anyhow!("missing command sub option"))?
+            .resolved
+            .as_ref()
+            .ok_or(anyhow!("missing command sub option value"))?;
+
+        let number = match option {
+            ApplicationCommandInteractionDataOptionValue::String(s) => s,
+            _ => return Err(anyhow!("wrong value type for command sub option")),
+        };
+
+        let quote = self.find_quote_by_number(number.parse::<i64>()?).await?;
+        match quote {
+            None => Ok(Some("Pas de résultat!".to_string())),
+            Some(q) => Ok(Some(q)),
+        }
+    }
+
+    async fn trigger_random(&self) -> Result<Option<String>, Error> {
+        let count = self.count().await?;
+        if count <= 0 {
+            Ok(None)
+        } else {
+            let offset = rand::thread_rng().gen_range(0..count);
+            let quote = sqlx::query_as::<_, Quote>("SELECT * FROM Quote LIMIT 1 OFFSET ?")
+                .bind(&offset)
+                .fetch_one(&*self.db_pool)
+                .await?;
+            Ok(Some(quote.quote))
+        }
+    }
 }
 
 #[async_trait]
@@ -41,7 +90,7 @@ impl SlashCommand for QuoteCommand {
             .create_option(|option| {
                 option
                     .name("get")
-                    .description("get")
+                    .description("trouver une citation avec son identifiant")
                     .kind(ApplicationCommandOptionType::SubCommand)
                     .create_sub_option(|sub_option| {
                         sub_option
@@ -50,6 +99,12 @@ impl SlashCommand for QuoteCommand {
                             .kind(ApplicationCommandOptionType::String)
                             .required(true)
                     })
+            })
+            .create_option(|option| {
+                option
+                    .name("random")
+                    .description("une citation au hasard")
+                    .kind(ApplicationCommandOptionType::SubCommand)
             });
     }
 
@@ -61,27 +116,16 @@ impl SlashCommand for QuoteCommand {
             return Ok(None);
         }
 
-        let option = &interaction
+        let command = interaction
             .data
             .options
             .get(0)
-            .ok_or(anyhow!("missing command option"))?
-            .options
-            .get(0)
-            .ok_or(anyhow!("missing command sub option"))?
-            .resolved
-            .as_ref()
-            .ok_or(anyhow!("missing command sub option value"))?;
+            .ok_or(anyhow!("missing command option"))?;
 
-        let number = match option {
-            ApplicationCommandInteractionDataOptionValue::String(s) => s,
-            _ => return Err(anyhow!("wrong value type for ommand sub option")),
-        };
-
-        let quote = self.find_quote_by_number(number.parse::<i64>()?).await?;
-        match quote {
-            None => Ok(Some("Pas de résultat!".to_string())),
-            Some(q) => Ok(Some(q)),
+        match command.name.as_str() {
+            "get" => self.trigger_get(command).await,
+            "random" => self.trigger_random().await,
+            e => Err(anyhow!("unknown command {}", e)),
         }
     }
 }
@@ -128,6 +172,10 @@ impl SlashCommand for QuoteAddCommand {
         &self,
         interaction: &ApplicationCommandInteraction,
     ) -> Result<Option<String>, Error> {
+        if interaction.data.name != "Add Quote" {
+            return Ok(None);
+        }
+
         let message = interaction
             .data
             .resolved
